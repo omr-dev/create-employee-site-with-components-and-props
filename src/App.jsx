85f978fc82a2f6865b4c4d740198c53575ada760
omer <peynirciomerfaruk@gmail.com>
2022-05-09 14:13:42 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Header } from "./components/Header";
 import { Employee } from "./components/Employee";
 import { Footer } from "./components/Footer";
 
+
 function App() {
+  const userIsExternal=false;
   return (
     <div className="App">
       <h1>Employee Site</h1>
-      <Header />
-      <Employee />
-      <Footer />
+      <Header userIsExternal={userIsExternal}/>
+      <Employee userIsExternal={userIsExternal}/>
+      <Footer userIsExternal={userIsExternal}/>
     </div>
   );
 }
